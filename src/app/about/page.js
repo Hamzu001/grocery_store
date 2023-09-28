f074e239e0f5b9be980939e01c4c973175a000d1
Hamza Zahid <zahidhamza001@gmail.com>
2023-09-27 22:49:22 -0700

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div className='text-center container'>
+    <h1 >
+      About page Next JS
+    </h1>
+  </div>
+  )
+}
+
+export default about
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='text-center container'>
+      <h1 >
+        Haloo Next JS
+      </h1>
+    </div>
+  )
+}
