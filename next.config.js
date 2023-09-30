8f05373e99266bf664dc683e4f7d0d267598baa5
Hamza Zahid <zahidhamza001@gmail.com>
2023-09-30 11:38:17 -0700

    comp

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        DB_LOCAL_URI:"mongodb://0.0.0.0:27017/grocery",
+        DB_LOCAL_URI:"mongodb://0.0.0.0:27017/grocery"
     }
 }
 
