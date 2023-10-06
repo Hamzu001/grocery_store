0bec48567758c17133b442acd71253e9994aee59
Hamza Zahid <zahidhamza001@gmail.com>
2023-10-06 11:17:26 -0700

    search

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -46,7 +46,7 @@ const Search = () => {
               value={query}
               type="search"
               id="search"
-              className="block w-full p-3 pl-10 text-sm  border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block w-full p-3 pl-10 text-sm  border border-gray-300 text-black rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search"
               required
             />
